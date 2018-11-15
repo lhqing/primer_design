5c62320cde52e41c81fbf9b23e8c3af9d0149a6d
lhqing <liuhq@ucsd.edu>
2018-11-14 22:56:37 -0800

    add blast part

---
+++ blast.py
@@ -0,0 +1,10 @@
+"""
+Author Hanqing Liu
+
+This file contain functions for using blast to check primer3 results
+
+Input:
+- output from primer.py
+- blast db corresponding to the bed file
+- Config file for blast
+"""
